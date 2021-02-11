9eacee66d49a1d25e7fa90dfc1eb9cda54b7bc58
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-02-11 06:19:54 -0500

    more commands

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 
-
-
 const client = new Discord.Client();
 
 const prefix = "!";
 
-client.on("message", function(message) { 
-   if(message.author.bot) return;                                      
-   if(!message.content.startsWith(prefix)) return
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-   const commandBody = message.content.slice(prefix.length);
-   const args = commandBody.split(' ');
-   const command = args.shift().toLowerCase();
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
 
    // All Commands - Long If Conditional
    //
 
-   if(command === "ping"){
-       const timeTaken = Date.now() - message.createdTimestamp;
-       message.reply(`Pong! This message had a latency of ${timeTaken}ms.`)
-   }
 
-   else if(command === "sum"){
-       const numArgs = args.map(x => parseFloat(x));
-       const sum = numArgs.reduce((counter, x) => count += x);
-       message.reply(`The sum of all the arguments you provided is ${sum}`)
-   }
-});                                      
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+
+  else if (command === "sum") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`The sum of all the arguments you provided is ${sum}!`);
+  }
+
+  else if(command === "push"){
+      message.reply(`Friendly Reminder to push code to your github today!`)
+  }
+
+  else if(command === "pushed"){
+      message.reply(`Hooray ${user}!!! I Believe In You!!! `)
+  }
+
+});
 
 client.login(config.BOT_TOKEN);
